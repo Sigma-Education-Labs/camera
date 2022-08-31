16c3b5428a6f6282ef23a24bd76f8f6b235d9100
Vasilen Alexandrov <vasilen.alexandrov@gmail.com>
2022-08-31 14:10:17 +0300

    Small optimizations

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from turtle import down
 from fire_detection import image_contains_fire
 import cv2
+import datetime
+import time
+
+start_time = time.time()
 
 test_image_paths = [
     "test_images/clouds.jpg",
@@ -18,7 +23,34 @@ test_image_paths = [
     "test_images/2.tif",
 ]
 
+def downlink(lat, long):
+    print("Detected fire on Lat: {} and Long: {}".format(lat, long))
 
 for i in test_image_paths:
     test_image = cv2.imread(i)
-    print("Fire on image {}: {}".format(i, image_contains_fire(test_image)))
+    print(test_image.shape)
+    print("Processing image {}".format(i))
+    
+    # Define the window size
+    windowsize_r = 240
+    windowsize_c = 180
+
+    # windowsize_r = 180
+    # windowsize_c = 135
+
+    ''' Broad phase '''
+    if (image_contains_fire(test_image)):
+        ''' Narrow phase '''
+
+        # Crop out a patch from the image and detect if there is fire in it
+        for r in range(0,test_image.shape[0] - windowsize_r, windowsize_r):
+            for c in range(0,test_image.shape[1] - windowsize_c, windowsize_c):
+                # print("Processing image patch {}:{}".format(r, c))
+                window = test_image[r:r+windowsize_r,c:c+windowsize_c]
+                if (image_contains_fire(window)):
+                    lat = 0.0
+                    long = 0.0
+                    print("Detected fire image patch from image {} on patch {}:{} with lat {} and long {}".format(i, r, c, lat, long))
+                    downlink(lat, long)
+
+print("--- %s seconds ---" % (time.time() - start_time))
