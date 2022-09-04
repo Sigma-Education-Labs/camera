d96a1fe941b69aa5685b086f371e2470ec14cf7f
Vasilen Alexandrov <vasilen.alexandrov@gmail.com>
2022-09-04 19:44:37 +0300

    Refactoring

---
+++ file_io.py
@@ -0,0 +1,31 @@
+import subprocess
+import os 
+
+def downlink(transfer_dir):
+    process = subprocess.run(['mv', 'fires.txt', transfer_dir])
+    return process
+
+def add_to_downlink_file(lat, long):
+    print("Detected fire on Lat: {} and Long: {}".format(lat, long))
+    f = open("fires.txt", "a")
+    f.write("{} {}\n".format(lat, long))
+    f.close()
+
+def get_latest_image_path(dirpath, valid_extensions=("raw", "jpg", "bmp", "tif", "tiff")):
+    """
+    Get the latest image file in the given directory
+    """
+
+    # get filepaths of all files and dirs in the given dir
+    valid_files = [os.path.join(dirpath, filename) for filename in os.listdir(dirpath)]
+    # filter out directories, no-extension, and wrong extension files
+    valid_files = [f for f in valid_files if '.' in f and \
+        f.rsplit('.',1)[-1] in valid_extensions and os.path.isfile(f)]
+    
+    if valid_files:
+        return max(valid_files, key=os.path.getmtime) 
+
+
+def delete_image_file(img_path):
+    process = subprocess.run(['rm', img_path])
+    return process
--- main.py
+++ main.py
@@ -1,41 +1,10 @@
-from fire_detection import image_contains_fire
+import fire_detection
 import cv2
 import time
 from telemetry.telemetry_main import get_obc_telemetry, get_patch_coords
 import subprocess
 from datetime import datetime
-import os
-
-testing = True
-tmp_dir = "/tmp"
-transfer_dir = "/work/transfer"
-
-def downlink():
-    with open('error_log.txt', 'w') as f:
-        process = subprocess.run(['mv','fires.txt', transfer_dir], stdout=f,universal_newlines=True)
-    return process
-
-def add_to_downlink_file(lat, long):
-    print("Detected fire on Lat: {} and Long: {}".format(lat, long))
-    f = open("fires.txt", "a")
-    f.write("{} {}\n".format(lat, long))
-    f.close()
-
-def get_latest_image_path(dirpath, valid_extensions=("raw", "jpg", "bmp", "tif", "tiff")):
-    """
-    Get the latest image file in the given directory
-    """
-
-    # get filepaths of all files and dirs in the given dir
-    valid_files = [os.path.join(dirpath, filename) for filename in os.listdir(dirpath)]
-    # filter out directories, no-extension, and wrong extension files
-    valid_files = [f for f in valid_files if '.' in f and \
-        f.rsplit('.',1)[-1] in valid_extensions and os.path.isfile(f)]
-
-    if not valid_files:
-        raise ValueError("No valid images in %s" % dirpath)
-
-    return max(valid_files, key=os.path.getmtime) 
+import file_io
 
 def image_processing(file_path, unix_time):
     test_image = cv2.imread(file_path)
@@ -47,16 +16,16 @@ def image_processing(file_path, unix_time):
     windowsize_c = 180
 
     ''' Broad phase '''
-    if (image_contains_fire(test_image)):
+    if (fire_detection.image_contains_fire(test_image)):
         ''' Narrow phase '''
         # Crop out a patch from the image and detect if there is fire in it
         for r in range(0,test_image.shape[0] - windowsize_r, windowsize_r):
             for c in range(0,test_image.shape[1] - windowsize_c, windowsize_c):
                 window = test_image[r:r+windowsize_r,c:c+windowsize_c]
-                if (image_contains_fire(window)):
+                if (fire_detection.image_contains_fire(window)):
                     lat, long = get_patch_coords(r,c,unix_time)
                     print("Detected fire image patch from image {} on patch {}:{} with lat {} and long {}".format(file_path, r, c, lat, long))
-                    add_to_downlink_file(lat, long)
+                    file_io.add_to_downlink_file(lat, long)
 
 def image_capture():
     with open('error_log.txt', 'w') as f:
@@ -70,17 +39,22 @@ def image_capture():
                         
 if __name__ == "__main__":
     start_time = time.time()
+    testing = True
+    tmp_dir = "/tmp"
+    # transfer_dir = "/work/transfer"
+    transfer_dir = "."
 
     while True:
         unix_time, opmode = get_obc_telemetry()
-        if(opmode == 4):
+        if opmode == 4:
             #satellite is in payload mode so call payload functions
             capture_error = image_capture()
             print(capture_error)
-            latest_img_path = get_latest_image_path(tmp_dir)
-            # time.sleep(8) #wait for image to be sent to dir
-            image_processing(latest_img_path, unix_time)
-            downlink()
+            latest_img_path = file_io.get_latest_image_path(tmp_dir)
+            if latest_img_path:
+                image_processing(latest_img_path, unix_time)
+                file_io.downlink(transfer_dir)
+                file_io.delete_image_file(latest_img_path)
         else:
             time.sleep(30)
             break
