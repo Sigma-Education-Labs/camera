e5f43a315bb518673b57e0ed9851a668f47091d7
Vasilen Alexandrov <vasilen.alexandrov@gmail.com>
2022-09-08 13:37:32 +0300

    Incorporating changes to the image capturing that were required by IBM

--- main.py
+++ main.py
@@ -7,6 +7,27 @@ from datetime import datetime
 import file_io
 from pathlib import Path
 import os
+import re
+import json
+
+def get_picture(camera_client, job_path):
+    camera_exec = subprocess.Popen([camera_client, "camera", "--capture", job_path, "-t", "jpg", "-p", "1"], stdout=subprocess.PIPE)
+    camera_response_str = camera_exec.communicate()[0].decode("utf-8")
+    p = re.compile('{\'status\': .*')
+    camera_response_jsons = p.findall(camera_response_str)
+    if len(camera_response_jsons) == 0:
+        print("Camera Error: " + camera_response_str)
+        return
+    q = re.compile('\'')
+    camera_json_str = q.sub('"', camera_response_jsons[0])
+    camera_json = json.loads(camera_json_str)
+    print(camera_json)
+    image_file = camera_json['value']
+    try:
+        if os.path.isfile(image_file):
+            os.rename(image_file, job_path + "image.jpg" )
+    except:
+        pass
 
 def image_processing(file_path, unix_time):
     test_image = cv2.imread(file_path)
@@ -31,17 +52,17 @@ def image_processing(file_path, unix_time):
                     print("Detected fire image patch from image {} on patch {}:{} with lat {} and long {} and prob {}".format(file_path, r, c, lat, long, probability_in_window))
                     file_io.add_to_downlink_file(lat, long, probability_in_window)
                     cv2.imwrite("result/fire_{}_{}_{}.jpg".format(r,c,unix_time),window)
-
-def image_capture():
-    with open('error_log.txt', 'w') as f:
-        process = subprocess.run(['es_rpiMgrClient','camera', '--capture', tmp_dir, '-t', 'jpg', '-p', '1'], stdout=f,universal_newlines=True)
-        return process
-
-                        
+        
 if __name__ == "__main__":
     start_time = time.time()
     tmp_dir = "/tmp"
     transfer_dir = "result"
+
+
+    # Create "result" dir if it doesn't exists
+    isExist = os.path.exists(transfer_dir)
+    if not isExist:
+        os.makedirs(transfer_dir)
 
     # Create fires.txt file
     transfer_fle = Path(os.path.join(transfer_dir, "fires.txt"))
@@ -53,8 +74,7 @@ if __name__ == "__main__":
 
     while True:
         unix_time = unix_time_now()
-        capture_error = image_capture()
-        print(capture_error)
+        get_picture("/usr/bin/es_rpiMgrClient", tmp_dir)
         latest_img_path = file_io.get_latest_image_path(tmp_dir)
         if latest_img_path:
             image_processing(latest_img_path, unix_time)
